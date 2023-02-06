1ae25a13daaa12d0606beb2183ff259a592ad068
ChrisKelton <ckelton@ufl.edu>
2023-02-06 18:05:47 -0500

    get parameter updating working

--- HW/HW2/cnn_mnist/cnn_mnist.py
+++ HW/HW2/cnn_mnist/cnn_mnist.py
@@ -11,7 +11,70 @@ from mlp_mnist.training_utils import Loss, Accuracy, save_accuracy_plot, save_lo
 from mlp_mnist.mlp import BaseMnist
 
 
-class CnnMnist(nn.Module, BaseMnist):
+class BaseCnnMnist(nn.Module):
+    def __init__(self):
+        super(BaseCnnMnist, self).__init__()
+        # CNN Layers
+        # first convolutional layers
+        # size of output after convolution operation uses the formula
+        # O = (I + 2*P - K)/S + 1, where O = output width/height
+        #                                I = input width/height
+        #                                P = width/height-wise padding size
+        #                                K = width/height of the convolution filter
+        #                                S = width/height-wise stride size
+        # I = 1 * 28 * 28
+        # P = 2
+        # K = 5 * 5
+        # S = 1
+        # O = (784 + 2*2 - 25)/1 + 1 = 764
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+        self.activation1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        # second convolutional layers
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=(5, 5), stride=(1, 1))
+        self.activation2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        # first fully-connected network
+        self.flatten = nn.Flatten()
+        self.linear3 = nn.Linear(in_features=400, out_features=120)
+        self.activation3 = nn.ReLU()
+
+        # second fully-connected network
+        self.linear4 = nn.Linear(in_features=120, out_features=84)
+        self.activation4 = nn.ReLU()
+
+        # third fully-connected network
+        self.linear5 = nn.Linear(in_features=84, out_features=10)
+
+        self.output6 = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        activation1 = self.activation1(conv1)
+        maxpool1 = self.maxpool1(activation1)
+
+        conv2 = self.conv2(maxpool1)
+        activation2 = self.activation2(conv2)
+        maxpool2 = self.maxpool2(activation2)
+
+        flattened = self.flatten(maxpool2)
+        linear3 = self.linear3(flattened)
+        activation3 = self.activation3(linear3)
+
+        linear4 = self.linear4(activation3)
+        activation4 = self.activation4(linear4)
+
+        linear5 = self.linear5(activation4)
+
+        return linear5
+
+    def class_probabilities(self, x):
+        return self.output6(x)
+
+
+class CnnMnist(BaseMnist):
     def __init__(
         self,
         n_epochs: int = 10,
@@ -23,75 +86,13 @@ class CnnMnist(nn.Module, BaseMnist):
         self.n_epochs = n_epochs
         self.epoch_to_save_model = epoch_to_save_model
         self.verbose = verbose
-
-        # CNN Layers
-        # first convolutional layers
-        # size of output after convolution operation uses the formula
-        # O = (I + 2*P - K)/S + 1, where O = output width/height
-        #                                I = input width/height
-        #                                P = width/height-wise padding size
-        #                                K = width/height of the convolution filter
-        #                                S = width/height-wise stride size
-
-        # I = 1 * 28 * 28
-        # P = 2
-        # K = 5 * 5
-        # S = 1
-        # O = (784 + 2*2 - 25)/1 + 1 = 764
-        self.conv0 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
-        self.activation0 = nn.ReLU()
-        self.maxpool0 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-
-        # second convolutional layers
-        self.conv1 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=(5, 5), stride=(1, 1))
-        self.activation1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-
-        # first fully-connected network
-        self.flatten = nn.Flatten()
-        self.linear2 = nn.Linear(in_features=400, out_features=120)
-        self.activation2 = nn.ReLU()
-
-        # second fully-connected network
-        self.linear3 = nn.Linear(in_features=120, out_features=84)
-        self.activation3 = nn.ReLU()
-
-        # third fully-connected network
-        self.linear4 = nn.Linear(in_features=84, out_features=10)
-
-        self.output5 = nn.Softmax(dim=1)
-
-        self.enc = OneHotEncoder()  # encoding scheme for labels
-        self.enc.fit([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])
+        self.cnn_mnist = BaseCnnMnist()
 
         self.loss_function = nn.CrossEntropyLoss()  # employs softmax
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        self.optimizer = torch.optim.Adam(self.cnn_mnist.parameters(), lr=learning_rate)
         self.output_decisions = 10
 
-    def forward(self, x):
-        conv0 = self.conv0(x)
-        activation0 = self.activation0(conv0)
-        maxpool0 = self.maxpool0(activation0)
-
-        conv1 = self.conv1(maxpool0)
-        activation1 = self.activation1(conv1)
-        maxpool1 = self.maxpool1(activation1)
-
-        flatten = self.flatten(maxpool1)
-        linear2 = self.linear2(flatten)
-        activation2 = self.activation2(linear2)
-
-        linear3 = self.linear3(activation2)
-        activation3 = self.activation3(linear3)
-
-        linear4 = self.linear4(activation3)
-
-        return linear4
-
-    def class_probabilities(self, x):
-        return self.output5(x)
-
-    def train_model(
+    def train(
         self,
         train_loader: torch.utils.data.DataLoader,
         output_path: Union[str, Path],
@@ -108,29 +109,30 @@ class CnnMnist(nn.Module, BaseMnist):
         epoch_tqdm = tqdm(total=self.n_epochs, desc="Epoch", position=0)
         for epoch in range(self.n_epochs):
             if test_loader is not None:
-                self.eval()
+                self.cnn_mnist.eval()
 
                 for test_batch_idx, test_data in enumerate(test_loader):
                     test_inputs, test_targets = test_data
                     # unsqueeze so dimensions match what CNN anticipates
                     # Will become (batch_size, 1 [channels], 28, 28)
-                    test_outputs = self.forward(torch.unsqueeze(test_inputs, dim=1))
+                    test_outputs = self.cnn_mnist.forward(torch.unsqueeze(test_inputs, dim=1))
                     loss_val = self.loss_function(test_outputs, test_targets)
                     test_loss += loss_val.item()
-                    output_class_probabilities = self.class_probabilities(test_outputs)
+                    output_class_probabilities = self.cnn_mnist.class_probabilities(test_outputs)
                     test_accuracy.compare_batch(targets=test_targets, outputs=output_class_probabilities)
                 test_accuracy.update_for_epoch()
                 test_loss.update_for_epoch()
-                self.train()
+                self.cnn_mnist.train()
 
             for batch_idx, train_data in enumerate(train_loader):
                 train_inputs, train_targets = train_data
                 self.optimizer.zero_grad()
-                train_outputs = self.forward(torch.unsqueeze(train_inputs, dim=1))
+                train_outputs = self.cnn_mnist.forward(torch.unsqueeze(train_inputs, dim=1))
                 loss_val = self.loss_function(train_outputs, train_targets)
+                loss_val.backward()
                 self.optimizer.step()
                 train_loss += loss_val.item()
-                output_class_probabilities = self.class_probabilities(train_outputs)
+                output_class_probabilities = self.cnn_mnist.class_probabilities(train_outputs)
                 train_accuracy.compare_batch(targets=train_targets, outputs=output_class_probabilities)
             train_accuracy.update_for_epoch()
             train_loss.update_for_epoch()
@@ -155,7 +157,7 @@ class CnnMnist(nn.Module, BaseMnist):
                     accuracy_plot_path,
                     loss_plot_path,
                 ) = self.get_output_paths_(output_path, epoch)
-                torch.save(self.state_dict(), epoch_output_path)
+                torch.save(self.cnn_mnist.state_dict(), epoch_output_path)
                 train_loss.save_json(train_loss_json_path)
                 train_accuracy.save_json(train_accuracy_json_path)
                 train_accuracy.save_confusion_matrix(confusion_matrix_path, "0123456789")
@@ -182,7 +184,7 @@ class CnnMnist(nn.Module, BaseMnist):
             loss_plot_path,
         ) = self.get_output_paths_(output_path, epoch)
         print(f"Saving model to '{output_path}'")
-        torch.save(self.state_dict(), output_path)
+        torch.save(self.cnn_mnist.state_dict(), output_path)
 
         self.train_loss = train_loss
         self.train_accuracy = train_accuracy
@@ -232,7 +234,7 @@ def cnn_mnist_run(
             test_loader = None
         train_images, train_labels = mndata.load_training()
         train_loader = cnn_mnist.preprocess_data(train_images, train_labels, batch_size_train)
-        cnn_mnist.train_model(train_loader, get_real_path(model_output_base_path), test_loader)
+        cnn_mnist.train(train_loader, get_real_path(model_output_base_path), test_loader)
 
 
 def main():
